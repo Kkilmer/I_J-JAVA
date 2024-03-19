f257423b0560bad5131fba70ef87ecfeca804223
Kkilmer <Kp827767@gmail.com>
2024-03-18 21:14:54 -0300

    construtor

--- att6/ex1/Main2.java
+++ att6/ex1/Main2.java
@@ -1,6 +1,6 @@
 package ex1;
 
-public class Main {
+public class Main2 {
     public static void main(String[] args) {
         Aluno a = new Aluno();
     }
--- const.henr6/src/nota_de_aula/Main.java
+++ const.henr6/src/nota_de_aula/Main.java
@@ -4,9 +4,11 @@ public class Main {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         Aluno2 a = new Aluno2("Kevin", 23, 10135);
-        Professor p = new Professor("Fernanda", 35, 3.000);
+        Professor p = new Professor("Fernanda", 35, 3500.0);
 
-        System.out.println("Nome do Aluno:"+ a.getNome() +
-                "Idade:"+ a.getIdade() + "Matricula:"+ a.getMatricula());
+        System.out.println("Nome do Aluno:" + " " + a.getNome() + " " +
+                "Idade:" + " " + a.getIdade() + " " + "Matricula: "+ " " + a.getMatricula());
+        System.out.println("Nome do Professor:" + " " + p.getNome() + " " + "Idade: " + p.getIdade() +
+                " " + "Salario: " + p.getSalario());
     }
 }
