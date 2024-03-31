caf0761713687a11c66104c10b71208167ef6c24
Kkilmer <kp827767@gmail.com>
2024-03-31 17:29:47 -0300

    Teste de Push

--- nota_de_aula_04/src/ex3/MainC.java
+++ nota_de_aula_04/src/ex3/MainC.java
@@ -3,7 +3,8 @@ import java.util.Scanner;
 public class MainC {
     public static void main(String[]args){
         Scanner sc = new Scanner(System.in);
-        Carro car = new Carro("Toyota", "Corolla");
+        Carro car = new Carro("Toyot", "Corolla");
         car.mostrarDetalhes();
+
     }
 }
